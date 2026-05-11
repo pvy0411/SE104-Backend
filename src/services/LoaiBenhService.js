5c7b6f29a7d8f1e965a1027c936e39f62a3dbbfc
pvy0411 <23521835@gm.uit.edu.vn>
2026-05-11 16:22:24 +0700

    fix

--- src/services/LoaiBenhService.js
+++ src/services/LoaiBenhService.js
@@ -1,19 +1,28 @@
 const LoaiBenhRepo = require('../repositories/LoaiBenhRepo');
 
 class LoaiBenhService {
-    async getDanhSachLoaiBenh() {
+    async getAll() {
         return await LoaiBenhRepo.getAll();
     }
 
-    async taoChanDoan(data) {
-        await LoaiBenhRepo.addChanDoan(
-            data.MaPK, 
-            data.MaLoaiBenh, 
-            data.TrieuChung, 
-            data.GhiChu
-        );
-        return data;
+    async create(tenBenh) {
+        if (!tenBenh) throw { status: 400, message: 'Vui lòng cung cấp tên loại bệnh!' };
+        return await LoaiBenhRepo.create(tenBenh);
+    }
+
+    async update(maLoaiBenh, tenBenh) {
+        if (!tenBenh) throw { status: 400, message: 'Vui lòng cung cấp tên loại bệnh mới!' };
+        await LoaiBenhRepo.update(maLoaiBenh, tenBenh);
+        return { message: 'Cập nhật thành công!' };
+    }
+
+    async delete(maLoaiBenh) {
+        const daSuDung = await LoaiBenhRepo.checkDaSuDung(maLoaiBenh);
+        if (daSuDung) {
+            throw { status: 400, message: 'Loại bệnh này đã có trong hồ sơ bệnh án, không thể xóa!' };
+        }
+        await LoaiBenhRepo.remove(maLoaiBenh);
+        return { message: 'Đã xóa loại bệnh!' };
     }
 }
-
 module.exports = new LoaiBenhService();
